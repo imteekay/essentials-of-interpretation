b7daa1516ceb8d2925dbd4f0b96578dbab94e6e3
imteekay <leandrotk100@gmail.com>
2023-08-03 08:06:53 -0300

    *

--- eva/__tests__/user-defined-function-test.js
+++ eva/__tests__/user-defined-function-test.js
@@ -1,24 +1,31 @@
 const { test } = require('./test-util');
 
 module.exports = (eva) => {
-  test(eva,
+  test(
+    eva,
     `(begin
       (def square (x)
         (* x x))
       
       (square 2)
-    )`, 4);
+    )`,
+    4,
+  );
 
-
-  test(eva,
+  test(
+    eva,
     `(begin
-      (var x 10)
-      (def foo () x)
-      (def bar ()
+      (var value 100)
+      (def calc (x y)
         (begin
-          (var x 20)
-          (+ (foo) x)))
+          (var z (+ x y))
+          (def inner (w)
+            (+ (+ w z) value))))
+          inner
       
-      (bar)
-    )`, 30);
+      (var fn (calc 10 20))
+      (fn 30)
+    )`,
+    160,
+  );
 };
