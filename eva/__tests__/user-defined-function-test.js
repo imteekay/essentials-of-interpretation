a7d26e82c42c8c6fd68851ecba98834c6f267209
imteekay <leandrotk100@gmail.com>
2023-08-05 09:52:32 -0300

    *

--- eva/__tests__/user-defined-function-test.js
+++ eva/__tests__/user-defined-function-test.js
@@ -28,4 +28,17 @@ module.exports = (eva) => {
     )`,
     160,
   );
+
+  test(
+    eva,
+    `(begin
+      (def factorial (x)
+        (if (= x 1)
+          1
+          (* x (factorial (- x 1)))))
+
+      (factorial 5)
+    )`,
+    120,
+  );
 };
