ce1b6745f97dbb18d8ebe4275295d6ec66c0c427
TK <leandrotk100@gmail.com>
2023-10-16 07:26:03 -0300

    *

--- eva/__tests__/block-test.js
+++ eva/__tests__/block-test.js
@@ -2,23 +2,23 @@ const assert = require('assert');
 const { test } = require('./test-util');
 
 module.exports = (eva) => {
-  assert.strictEqual(
-    eva.eval([
-      'begin',
-      ['var', 'x', 10],
-      ['var', 'y', 20],
-      ['+', ['*', 'x', 'y'], 30],
-    ]),
+  test(
+    eva,
+    `(begin
+      (var x 10)
+      (var y 20)
+      (+ (* x y) 30))`,
     230,
   );
 
-  assert.strictEqual(
-    eva.eval([
-      'begin',
-      ['var', 'x', 10],
-      ['begin', ['var', 'x', 20], 'x'],
-      'x',
-    ]),
+  test(
+    eva,
+    `(begin
+      (var x 10)
+      (begin
+        (var x 10)
+        x)
+      x)`,
     10,
   );
 
