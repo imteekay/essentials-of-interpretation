00d0326c128e685437be319f46a50c490eea27c5
TK <leandrotk100@gmail.com>
2023-10-16 07:54:59 -0300

    *

--- eva/__tests__/built-in-function-test.js
+++ eva/__tests__/built-in-function-test.js
@@ -1,12 +1,10 @@
 const { test } = require('./test-util');
 
 module.exports = (eva) => {
-  // math
   test(eva, `(+ 1 5)`, 6);
   test(eva, `(+ (+ 2 3) 5)`, 10);
   test(eva, `(+ (* 2 3) 5)`, 11);
 
-  // comparison
   test(eva, `(> 1 5)`, false);
   test(eva, `(< 1 5)`, true);
   test(eva, `(>= 5 5)`, true);
