df65266cc3dd1f64c3940defe33de693d284c094
TK <leandrotk100@gmail.com>
2023-10-17 07:24:35 -0300

    *

--- eva/__tests__/if-expression-test.js
+++ eva/__tests__/if-expression-test.js
@@ -1,16 +1,15 @@
-const assert = require('assert');
+const { test } = require('./test-util');
 
 module.exports = (eva) => {
-  assert.strictEqual(
-    eva.eval([
-      'begin',
-      ['var', 'x', 10],
-      ['var', 'y', 0],
-
-      ['if', ['>', 'x', 10], ['set', 'y', 20], ['set', 'y', 30]],
-
-      'y',
-    ]),
+  test(
+    eva,
+    `(begin
+      (var x 10)
+      (var y 0)
+      (if (> x 10)
+        (set y 20)
+        (set y 30))
+      y)`,
     30,
   );
 };
