bf6ca9c1406bd4e9e69d0600001e7b737bde3774
TK <leandrotk100@gmail.com>
2023-10-17 07:41:42 -0300

    *

--- eva/__tests__/while-expression-test.js
+++ eva/__tests__/while-expression-test.js
@@ -1,24 +1,18 @@
-const assert = require('assert');
+const { test } = require('./test-util');
 
 module.exports = (eva) => {
-  assert.strictEqual(
-    eva.eval([
-      'begin',
-      ['var', 'counter', 0],
-      ['var', 'result', 0],
-
-      [
-        'while',
-        ['<', 'counter', 10],
-        [
-          'begin',
-          ['set', 'counter', ['+', 'counter', 1]],
-          ['set', 'result', ['+', 'result', 1]],
-        ],
-      ],
-
-      'result',
-    ]),
+  test(
+    eva,
+    `(begin
+      (var counter 0)
+      (var result 0)
+      
+      (while (< counter 10)
+        (begin
+          (set counter (+ counter 1))
+          (set result (+ result 1))))
+          
+      result)`,
     10,
   );
 };
